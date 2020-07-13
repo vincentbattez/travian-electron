1b81d045a8b505a2322d202fb3f6266ff9c82723
Vincent Battez <vincent.battez.pro@gmail.com>
2020-07-13 17:43:32 +0200

    refactor + add mouvement HP + mouvement

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -4,10 +4,25 @@ function timerToMs(timerElement) {
   return ((timer[0] * 3600) + (timer[1] * 60) + timer[2])
 }
 
-function notificationFormat({ iftttUrl, name, timeOut, action }) {
+function notificationFormat(notifObj) {
+  if(!notifObj) {
+    console.log('nothing to do');
+    return
+  }
+  const { iftttUrl, name, timeOut, action } = notifObj
+
   console.table([iftttUrl, name, timeOut, action]);
 
   return `${iftttUrl}|${name}|${timeOut}|${action}`
+}
+
+function notificationObjectFormat({ iftttUrl, name, timeOut, action }) {
+  return {
+    iftttUrl,
+    name,
+    timeOut,
+    action
+  }
 }
 
 function getIFTTTUrl(trigger) {
@@ -16,41 +31,69 @@ function getIFTTTUrl(trigger) {
   return `https://maker.ifttt.com/trigger/${trigger}/with/key/${iftttKey}`
 }
 
+function buildNotification(action, nameElement, timerElement) {
+  console.log('buildNotification - ' + action);
 
-
-function buildingNotification() {
-  console.log('buildingNotification');
-
-  const action = 'building_done'
-  const iftttUrl = getIFTTTUrl(action)
-  
-  const name = document.querySelector('.boxes-contents .name').innerText
-  const timerElement = document.querySelector('.boxes-contents .timer')
-  
+  const iftttUrl = getIFTTTUrl(action)  
+  const name = nameElement.innerText
   const timeOut = timerToMs(timerElement)
   
-  return notificationFormat({iftttUrl, name, timeOut, action})
-}
-
-function needRessourceNotification() {
-  console.log('needRessourceNotification');
-
-  const action = 'need_ressources'
-  const iftttUrl = getIFTTTUrl(action)
-  
-  const name = document.querySelector('.titleInHeader').innerText
-  const timerElement = document.querySelector('.upgradeBlocked .timer')
-  
-  const timeOut = timerToMs(timerElement)
-  
-  return notificationFormat({iftttUrl, name, timeOut, action})
+  return notificationObjectFormat({iftttUrl, name, timeOut, action})
 }
 
 // app
-if(document.querySelector('.boxes-contents .timer')) {
-  buildingNotification()
-} else if (document.querySelector('.upgradeBlocked .timer')) {
-  needRessourceNotification()
-} else {
-  console.log('nothing to do');
+function getSoonNotification() {
+  const notificationCollection = []
+
+  // Building
+  if(document.querySelector('.buildingList .timer')) {
+    notificationCollection.push(
+      buildNotification(
+        'building_done',
+        document.querySelector('.buildingList .name'),
+        document.querySelector('.buildingList .timer'),
+      )
+    )
+  }
+
+  // Mouvement HP
+  if (document.querySelector('#movements .timer')) {
+    notificationCollection.push(
+      buildNotification(
+        'mouvement',
+        document.querySelector('#movements .timer'),
+        document.querySelector('#movements .timer'),
+      )
+    )
+  }
+
+  // Mouvement
+  if (document.querySelector('.troop_details .timer')) {
+    notificationCollection.push(
+      buildNotification(
+        'mouvement',
+        document.querySelector('.troopHeadline a'),
+        document.querySelector('.troop_details .timer'),
+      )
+    )
+  }
+
+  // Need more ressource
+  if (document.querySelector('.upgradeBlocked .timer')) {
+    notificationCollection.push(
+      buildNotification(
+        'need_ressources',
+        document.querySelector('.titleInHeader'),
+        document.querySelector('.upgradeBlocked .timer'),
+      )
+    )
+  }
+
+  const timeOutCollection = notificationCollection.map(notification => notification.timeOut)
+
+  return notificationCollection.find(notif => notif.timeOut === Math.min(...timeOutCollection))
 }
+
+console.log(getSoonNotification());
+
+notificationFormat(getSoonNotification()) || null
